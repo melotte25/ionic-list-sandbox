d25d83a66310fae96926d00e8cb7154a18017f14
Melotte25 <124006746+melotte25@users.noreply.github.com>
2023-02-22 19:26:58 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-list-sandbox',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
